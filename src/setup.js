37f284d31dcb3312d210425904b505ba560cbb99
Christian Ego <Christian.Ego@c-ego.net>
2018-06-03 18:16:24 +0200

    added postdeploy setup

---
+++ src/setup.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const fixtures = require('node-mongoose-fixtures');
+const Wine = require('./model/Wine');
+const Sequence = require('./model/Sequence');
+
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/wine-manager-test';
+
+if(NODE_ENV === 'production' || NODE_ENV === 'prod') {
+    console.warn("Setup is only intended to be executed in non production environment.");
+    return;
+}
+
+mongoose.connect(MONGODB_URI);
+mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error:'));
+
+function done() {
+    console.info("Setup finished.");
+}
+
+Wine.deleteMany({}, function (error) {
+    if (error) {
+        console.error(error);
+    }
+    console.info("Wine.deleteMany({}) done.");
+    Sequence.findOneAndUpdate({_id: 'WineSequence'}, {$set: {seq: 0}}, function (error) {
+        if (error) {
+            console.error(error);
+        }
+        console.info("Sequence.findOneAndUpdate({_id: 'WineSequence'}, {$set: {seq: 0}}) done.");
+        fixtures({
+            Wine: [
+                {
+                    id: 0,
+                    name: 'Cabernet sauvignon',
+                    year: 2013,
+                    country: 'France',
+                    type: 'red',
+                    description: 'The Sean Connery of red wines'
+                },
+                {
+                    id: 1,
+                    name: 'Pinot noir',
+                    year: 2011,
+                    country: 'France',
+                    type: 'red',
+                    description: 'Sensual and understated'
+                },
+                {
+                    id: 2,
+                    name: 'Zinfandel',
+                    year: 1990,
+                    country: 'Croatia',
+                    type: 'red',
+                    description: 'Thick and jammy'
+                }
+            ]
+        }, function (error) {
+            if (error) {
+                console.error(error);
+            }
+            console.info("fixtures done.");
+            done();
+        });
+
+    });
+});
